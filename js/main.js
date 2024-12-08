1af7e3395467991c21e32d2edb34a7bf31f7c6d5
Вячеслав <baranov_v_s1@mail.ru>
2024-12-09 04:39:17 +0500

    Задание Открывается и закрывается

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 import { createPhotoDescription } from './photo.js';
 import { renderPictures } from '.miniatureDrawing.js';
+import { renderFullSizePhoto } from './fullSizePhoto.js';
+
 renderPictures(createPhotoDescription());
+renderFullSizePhoto();
